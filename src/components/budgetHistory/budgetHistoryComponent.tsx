07271fc311912bb731bbc9d63bc832e91615dcc0
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-03-11 13:09:29 +0100

    fix: history and history finder fixed

--- src/components/budgetHistory/budgetHistoryComponent.tsx
+++ src/components/budgetHistory/budgetHistoryComponent.tsx
@@ -4,25 +4,8 @@ import "./budgetHistoryComponent.css";
 
 export const HistoryGenerator = () => {
   const [name, setName] = useState("");
-  const [foundBudgets, setFoundBudgets] = useState<Budget[]>([]);
-  const storedBudgets = localStorage.getItem("budgets");
-  const [hasSearched, setHasSearched] = useState(false);
-  let budgets: Budget[] = [];
-
-  if (storedBudgets) {
-    budgets = JSON.parse(storedBudgets) as Budget[];
-  }
-
-  const handleFind = () => {
-    const results = budgets.filter((b) => b.clientName.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
-    setFoundBudgets(results);
-    setHasSearched(true);
-  };
-
-  const allBudgets = () => {
-    setFoundBudgets(budgets);
-    setHasSearched(true);
-  };
+  const budgets: Budget[] = JSON.parse(localStorage.getItem("budgets") || "[]");
+  const [foundBudgets, setFoundBudgets] = useState<Budget[]>(budgets);
 
   return (
     <div className="history-generator">
@@ -32,27 +15,34 @@ export const HistoryGenerator = () => {
         type="text"
         placeholder="name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+        onChange={(e) => {
+          const value = e.target.value;
+          setName(value);
 
-      <button onClick={handleFind}>Find Budget With That Name</button>
-      <button onClick={allBudgets}>Find All Budgets</button>
+          if (value === "") {
+            setFoundBudgets(budgets);
+            return;
+          }
+
+          const results = budgets.filter((b) =>
+            b.clientName.toLowerCase().includes(value.toLowerCase()),
+          );
+
+          setFoundBudgets(results);
+        }}
+      />
+      <div></div>
 
       <div>
         {foundBudgets.map((b) => (
-              <div key={b.id} className="budget-item">
-                <p>Name: {b.clientName}</p>
-                <p>Email: {b.clientEmail}</p>
-                <p>Date: {new Date(b.createdAt).toLocaleDateString("es-ES")}</p>
-                <p>Services: {b.service.join(",")}</p>
-                <p>Total: {b.total} €</p>
-              </div>
-            ))}
-            
-{hasSearched && foundBudgets.length === 0 && (
-  <p className="no-budgets">No budgets found with that name</p>
-)}
-
+          <div key={b.id} className="budget-item">
+            <p>Name: {b.clientName}</p>
+            <p>Email: {b.clientEmail}</p>
+            <p>Date: {new Date(b.createdAt).toLocaleDateString("es-ES")}</p>
+            <p>Services: {b.service.join(",")}</p>
+            <p>Total: {b.total} €</p>
+          </div>
+        ))}
       </div>
     </div>
   );
